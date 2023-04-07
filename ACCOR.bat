4d8603d69288e364b723f216a42fc3595c126185
Fraiddd <frederick.coulon@gmail.com>
2023-04-07 16:26:53 +0200

    Guillemets

--- ACCOR.bat
+++ ACCOR.bat
@@ -1,2 +1,2 @@
-cd %~dp0
-for /f "delims=" %%f IN ('dir /b "*.dwg"') do accoreconsole.exe /i "%%f" /s %~n0.scr
+cd "%~dp0"
+for /f "delims=" %%f IN ('dir /b "*.dwg"') do accoreconsole.exe /i "%%f" /s "%~n0.scr"
