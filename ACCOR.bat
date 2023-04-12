73a6da5e61d899dd47888c0b5047e67ad165fd0e
Fraiddd <frederick.coulon@gmail.com>
2023-04-12 13:42:37 +0200

    Update

--- ACCOR.bat
+++ ACCOR.bat
@@ -1,3 +1,4 @@
+@echo off
 rem ACCOR.bat : Utilise accoreconsole.exe pour appliquer un scrpt à un dossier.
 rem Coller ce fichier à coté du .scr dans le dossier cible.
 rem Renommer ce fichier selon le .scr
